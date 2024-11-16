1743011729b46a469e06fb96ba2a0a43c002882a
obidyhasan <obidyhasan@gmail.com>
2024-11-16 11:21:56 +0600

    include firebase

--- src/routes/PrivateRouter.jsx
+++ src/routes/PrivateRouter.jsx
@@ -8,7 +8,6 @@ const PrivateRouter = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
 
   const location = useLocation();
-  console.log(location);
 
   if (loading) {
     return <Loading></Loading>;
